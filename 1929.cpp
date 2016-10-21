8f1b8747d9139b9eee081945f7c464202322d301
Byungwook Lee <quddnr153@gmail.com>
2016-10-21 18:27:36 +0900

    1929 complete

--- 1929.cpp
+++ 1929.cpp
@@ -1,43 +1,38 @@
-#include <iostream>
+nclude <iostream>
 #include <cstdio>
+#include <math.h>
+#include <vector>
+#include <algorithm>
 #include <string>
 #include <string.h>
-#include <math.h>
-#include <algorithm>
-#include <vector>
-#include <stdlib.h>
-
-#define MAX_N 1000*1000
 
 using namespace std;
 
-int n;
+const int MAX_N = 1000001;
+int n = MAX_N;
+unsigned char prime[(MAX_N + 7) / 8];
 
-unsigned char sieve[(MAX_N + 7) / 8];
-inline bool isPrime(int k) { return sieve[k >> 3] & (1 << (k & 7)); }
+inline bool isPrime(int k) { return prime[k >> 3] & (1 << (k & 7)); }
+inline void setcomposite(int k) { prime[k >> 3] &= ~(1 << (k & 7)); }
 
-inline void setComposite(int k) { sieve[k >> 3] &= ~(1 << (k & 7)); }
-
-void eratosthenes() {
-    n = MAX_N;
-    memset(sieve, 255, sizeof(sieve));
-    setComposite(0);
-    setComposite(1);
-    int sqrtn = int(sqrt((double)n));
-    for(int i = 2; i <= sqrtn; ++i)
-        if(isPrime(i))
-            for(int j = i *i; j <=n; j+=i)
-                setComposite(j);
+void eratosthenes(){
+	memset(prime, 255, sizeof(prime));
+	setcomposite(0);
+	setcomposite(1);
+	for(int i = 2; i * i <= n; i++){
+		if(isPrime(i))
+		for(int j = i * i; j <= n; j += i)
+			setcomposite(j);
+	}
 }
 
 int main()
 {
-    int M, N;
-    cin >> M >> N;
-    eratosthenes();
-    for(int i = M; i <= N; i++) {
-        if(isPrime(i))
-            cout << i << endl;
-    }
-    return 0;
+	int M, N;
+	cin >> M >> N;
+	eratosthenes();
+	for(int i = M; i <= N; i++){
+		if(isPrime(i)) printf("%d\n", i);
+	}
+	return 0;
 }
